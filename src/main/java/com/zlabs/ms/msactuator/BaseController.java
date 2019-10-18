cee53809ff0d43ef182e9f61a2279b5498c50a01
Zuriel Diaz <Zuriel.Diaz@Staples.com>
2019-10-17 21:52:43 -0500

    Refactor: controller simplification.

--- src/main/java/com/zlabs/ms/msactuator/BaseController.java
+++ src/main/java/com/zlabs/ms/msactuator/BaseController.java
@@ -8,7 +8,7 @@ public class BaseController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "init.";
+		return "index.";
 	}
 	
 }
--- src/main/java/com/zlabs/ms/msactuator/HelloWorldController.java
+++ src/main/java/com/zlabs/ms/msactuator/HelloWorldController.java
@@ -1,22 +1,15 @@
 package com.zlabs.ms.msactuator;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 	
 	@GetMapping("/hello-world")
-	@ResponseBody
-	public Greeting sayHello(@RequestParam(name="name",required=false,defaultValue="Stranger") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template,name));
+	public String sayHello(@RequestParam(name="name") String name) {
+		return "Hello " + name;
 	}
 	
 }
